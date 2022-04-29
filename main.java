19d15eacadfa7785c787b456f037b69f9de3eeb5
IshanA2007 <94868152+IshanA2007@users.noreply.github.com>
2022-04-29 10:14:01 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,13 @@
-    
+    import javax.swing.*;
+import java.awt.*;
+
+public class main{
+   public static void main(String[] args){
+      JFrame frame = new JFrame("ur mom");
+      frame.setSize(700, 700);
+      frame.setLocation(20, 20);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(new Game());
+      frame.setVisible(true);
+   }
+}    
