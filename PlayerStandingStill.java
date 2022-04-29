0e06b6350863ee0f3c8791e1fce0fc1f8bf953c2
IshanA2007 <94868152+IshanA2007@users.noreply.github.com>
2022-04-29 11:44:00 -0400

    works with game, and runs in main.
    we need to make this for other player movetypes as well (this will be easy)

--- PlayerStandingStill.java
+++ PlayerStandingStill.java
@@ -1,3 +1,33 @@
-public class PlayerStandingStill {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+
+public class PlayerStandingStill implements Player{
+   public int headX;
+   public int headY;
+   public int rectX;
+   public int rectY;
+   
+   public PlayerStandingStill(){
+      headX = 50;
+      headY = 325;
+      rectX = 50;
+      rectY = 350;
+   }
+   
+   public void draw(Graphics g){
+      g.setColor(Color.BLACK);
+      g.fillOval(headX, headY, 25, 25);
+      g.fillRect(rectX, rectY, 25, 50);
+   }
+   
+   public void move(int changeX, int changeY){
+      headX += changeX;
+      headY += changeY;
+      rectX += changeX;
+      rectY += changeY;
+   }
 }
+      
+
