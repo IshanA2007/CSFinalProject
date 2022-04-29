2916713d6ac24e3cf80db5dd62143e76f97b781e
IshanA2007 <94868152+IshanA2007@users.noreply.github.com>
2022-04-29 11:43:06 -0400

    player is an interface for all movements

--- Player.java
+++ Player.java
@@ -1,3 +1,10 @@
-public class Player {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+
+public interface Player {
+    public void move(int changeX, int changeY);
     
+    public void draw(Graphics g);
 }
