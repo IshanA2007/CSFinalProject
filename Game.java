abce024078b431fae23771b6274c473816af4be6
IshanA2007 <94868152+IshanA2007@users.noreply.github.com>
2022-04-29 11:42:37 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -11,6 +11,14 @@ public class Game extends JPanel{
    StartingBackground bckground;
    private boolean space;
    public int count = 0;
+   public int playerVelocityX = 0;
+   public int playerVelocityY = 0;
+   Player player;
+   
+   public boolean increaseVelocityUp = true;
+   public boolean increaseVelocityDown = true;
+   public boolean increaseVelocityRight = true;
+   public boolean increaseVelocityLeft = true;
    
    public Game(){
       myImage = new BufferedImage(700, 700, BufferedImage.TYPE_INT_RGB);
@@ -18,10 +26,13 @@ public class Game extends JPanel{
       myBuffer.setColor(Color.WHITE);   
       myBuffer.fillRect(0, 0, 700, 700);
       bckground = new StartingBackground(0);
+      player = new PlayerStandingStill();
+      
       
       t = new Timer(5, new AnimationListener());
       
-      
+      addKeyListener(new Key());
+      setFocusable(true);
       
       begin();
    }
@@ -32,15 +43,19 @@ public class Game extends JPanel{
    
    public void animate(){
       bckground.draw(myBuffer);
-      if(count < 400){
-         
-         bckground.moveBackground(5);
-         count++;
+      bckground.moveBackground(2*playerVelocityX);
+      /*
+      if(playerVelocityX > 0){
+         player = new PlayerMovingRight();
       }
-      else{
-         
-         bckground.moveBackground(-5);
+      else if(playerVelocityX < 0){
+         player = new PlayerMovingLeft();
       }
+      */
+      player.draw(myBuffer);
+      player.move(playerVelocityX, playerVelocityY);
+      
+      
       repaint();
    }
    
@@ -57,4 +72,42 @@ public class Game extends JPanel{
       }
    }
       
+   private class Key extends KeyAdapter{
+      public void keyPressed(KeyEvent e){
+         if(e.getKeyCode() == KeyEvent.VK_RIGHT && increaseVelocityRight){
+            playerVelocityX += 8;
+            increaseVelocityRight = false;
+         }
+         else if(e.getKeyCode() == KeyEvent.VK_LEFT && increaseVelocityLeft){
+            playerVelocityX -= 8;
+            increaseVelocityLeft = false;
+         }
+         else if(e.getKeyCode() == KeyEvent.VK_UP && increaseVelocityUp){
+            playerVelocityY -= 8;
+            increaseVelocityUp = false;
+         }
+         else if(e.getKeyCode() == KeyEvent.VK_DOWN && increaseVelocityDown){
+            playerVelocityY += 8;
+            increaseVelocityDown = false;
+         }
+      }
+      public void keyReleased(KeyEvent e){
+         if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            playerVelocityX -= 8;
+            increaseVelocityRight = true;
+         }
+         else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            playerVelocityX += 8;
+            increaseVelocityLeft = true;
+         }
+         else if(e.getKeyCode() == KeyEvent.VK_UP){
+            playerVelocityY += 8;
+            increaseVelocityUp = true;
+         }
+         else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            playerVelocityY -= 8;
+            increaseVelocityDown = true;
+         }
+      }
+   }
 }
