4137f22601d180b8e3320f799e071a2361b867fd
IshanA2007 <94868152+IshanA2007@users.noreply.github.com>
2022-05-02 08:52:26 -0400

    Update StartingBackground.java

--- StartingBackground.java
+++ StartingBackground.java
@@ -1,21 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.File;
+import java.awt.event.*;
+import java.io.IOException;
+
 public class StartingBackground{
     public int referenceX;
+    private BufferedImage image;
+    int w;
+    int h;
+    
     
     public StartingBackground(int newRefX){
+      try{
+         File riverImg = new File("river-removebg-preview.png");
+         image = ImageIO.read(riverImg);
+         w = image.getWidth(null);
+         h = image.getHeight(null);
+      }
+      catch (IOException e){
+         System.exit(1);
+      }
       referenceX = newRefX;
     }
     
    public void draw(Graphics g){
-      g.setColor(Color.GREEN);
+      //grass
+      g.setColor(new Color(4, 120, 33));
       g.fillRect(0, 0, 700, 700);
-      g.setColor(Color.RED);
-      g.fillRect(0, 300, 700, 100);
-      g.fillRect(400-referenceX, 0, 100, 700);
-     
-      g.fillRect(1100 - referenceX, 0, 100, 700);
-      g.fillRect(1300-referenceX, 0, 100, 700);
+      g.
+      g.drawImage(image, 500-referenceX, 0, w+700-referenceX, 700, 0, 0, w, h, null);
       
    }
    
