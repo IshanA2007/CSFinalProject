28c3ef26ae8f7787cd697148b6f09f21cf6503c0
IshanA2007 <94868152+IshanA2007@users.noreply.github.com>
2022-05-02 13:46:00 -0400

    Add files via upload

---
+++ Boundary.java
@@ -0,0 +1,15 @@
+public class Boundary{
+   public int corner1X;
+   public int corner1Y;
+   public int corner2X;
+   public int corner2Y;
+   
+   public Boundary(ncorner1X, ncorner1Y, ncorner2X, ncorner2Y){
+      corner1X = ncorner1X;
+      corner1Y = ncorner1Y;
+      corner2X = ncorner2X;
+      corner2Y = ncorner2Y;
+   }
+   
+}
+      
